cde0c01c28baf50652046f6af6ce8039ae536017
Oleh <olegflud5@gmail.com>
2020-06-10 19:07:42 +0300

    add nodemon

--- App.js
+++ App.js
@@ -22,13 +22,11 @@ app.use(morgan('dev'));
 
 app.use(fileUpload({}));
 
-
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.use('/static', express.static(__dirname+'/public'));
 global.appRoot = __dirname;
-
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
